193000a299250e354f3d76b045b4d8a1703355ae
Deepyaman Datta <deepyaman.datta@utexas.edu>
2022-07-29 18:01:39 -0400

    feat(argo): add a minimal, HTTP-only importer node

---
+++ exedra/backends/argo/pipeline_task.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+
+from argo_workflows.model.io_argoproj_workflow_v1alpha1_artifact import (
+    IoArgoprojWorkflowV1alpha1Artifact,
+)
+from argo_workflows.model.io_argoproj_workflow_v1alpha1_template import (
+    IoArgoprojWorkflowV1alpha1Template,
+)
+
+
+class PipelineTask:
+    def __init__(
+        self,
+        template: IoArgoprojWorkflowV1alpha1Template,
+        input_mapping: dict[str, IoArgoprojWorkflowV1alpha1Artifact],
+    ):
+        self.template = template
